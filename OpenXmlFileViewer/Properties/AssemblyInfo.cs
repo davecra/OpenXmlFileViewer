700a7ab66087f7ea36401db5b8821e17ee99b78b
David E. Craig <decyahoo-github@yahoo.com>
2020-08-07 10:08:55 -0400

    Updating assembly information

--- OpenXmlFileViewer/Properties/AssemblyInfo.cs
+++ OpenXmlFileViewer/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OpenXmlFileViewer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Office Open XML File Viewer")]
+[assembly: AssemblyDescription("Office Open XML File Viewer")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OpenXmlFileViewer")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCompany("davecra")]
+[assembly: AssemblyProduct("Office Open XML File Viewer")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
